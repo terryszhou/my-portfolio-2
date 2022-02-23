7ca20583b2d59f39068b241d02b6ba38b9399c88
terryszhou <terryszhou@gmail.com>
2022-02-23 12:37:15 -0600

    Animations added to exp list

--- src/components/ExperienceList.tsx
+++ src/components/ExperienceList.tsx
@@ -4,6 +4,7 @@ import * as React from "react";
 import { BiRightArrow } from "react-icons/bi";
 import { BsXDiamondFill } from "react-icons/bs";
 
+import { fadeDown } from "../helpers/animations";
 import { expArray } from "../helpers/variables";
 
 interface ExperienceListProps {
@@ -13,25 +14,57 @@ interface ExperienceListProps {
 export const ExperienceList = ({ idx }: ExperienceListProps) => {
   const atCompanyColor2: string = useColorModeValue("rgb(102,105,127)", "gray.400");
   const listColor2: string = useColorModeValue("rgb(78,83,104)", "gray.300");
+  const fadeDownAnim: string = `${fadeDown} 800ms`;
+  const fadeDownAnim2: string = `${fadeDown} 800ms 120ms forwards`;
+  const fadeDownAnim3: string = `${fadeDown} 800ms 240ms forwards`;
 
   return (
     <Box fontFamily={"var(--chakra-fonts-nunito)"}>
-      <Text fontWeight={"bold"}>{expArray[idx].title}</Text>
-      <Text as={"span"} color={atCompanyColor2} fontWeight="bold"> @ {expArray[idx].company}</Text>
-      <Text color={listColor2}>{expArray[idx].dates}</Text>
-      <List color={listColor2} marginY={5}>
+      <Box animation={fadeDownAnim}>
+        <Text fontWeight={"bold"}>
+          {expArray[idx].title}
+        </Text>
+        <Text
+          as={"span"}
+          color={atCompanyColor2}
+          fontWeight="bold">
+          @ {expArray[idx].company}
+        </Text>
+        <Text color={listColor2}>
+          {expArray[idx].dates}
+        </Text>
+      </Box>
+      <List
+        animation={fadeDownAnim2}
+        color={listColor2}
+        marginY={5}
+        opacity={0}>
         {expArray[idx].details.map((detail, i) => (
-          <ListItem key={i} marginY={2} display={"flex"}>
-            <ListIcon as={BsXDiamondFill} color='green.500' marginTop={1} />
-            {detail}
+          <ListItem
+            display={"flex"}
+            marginY={2}
+            key={i}>
+            <ListIcon
+              as={BsXDiamondFill}
+              color='green.500'
+              marginTop={1} />
+              {detail}
           </ListItem>
         ))}
       </List>
-      <List color={listColor2}>
+      <List
+        animation={fadeDownAnim3}
+        color={listColor2}
+        opacity={0}>
         {expArray[idx].skills.map((skill, i) => (
-          <ListItem key={i} display={"flex"} alignItems={"center"}>
-            <ListIcon as={BiRightArrow} color='green.500' />
-            {skill}
+          <ListItem
+            alignItems={"center"}
+            display={"flex"}
+            key={i}>
+            <ListIcon
+              as={BiRightArrow}
+              color='green.500' />
+              {skill}
           </ListItem>
         ))}
       </List>
