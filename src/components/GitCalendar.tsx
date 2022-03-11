e451128010cfc311a89bd3cbf3512afdba926b48
terryszhou <terryszhou@gmail.com>
2022-03-10 18:36:20 -0600

    light mode github calendar theme recolored

--- src/components/GitCalendar.tsx
+++ src/components/GitCalendar.tsx
@@ -17,10 +17,10 @@ export const GitCalendar = () => (
       color={"goldenrod"}
       theme={{
         level0: uCMV("rgb(200,200,200)","rgb(44,44,44)"),
-        level1: 'rgb(107,91,47)',
-        level2: 'rgb(137,115,31)',
-        level3: 'rgb(166,135,56)',
-        level4: 'goldenrod' }}
+        level1: uCMV('goldenrod','rgb(107,91,47)'),
+        level2: uCMV('rgb(176,145,66)','rgb(137,115,31)'),
+        level3: uCMV('rgb(147,125,41)','rgb(166,135,56)'),
+        level4: uCMV('rgb(117,101,57)','goldenrod') }}
       username="terryszhou">
       <ReactTooltip html />
     </GitHubCalendar>
